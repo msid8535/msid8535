afe05f5b6e7add8204c5b0f0e2cbee2bdcba35d7
mohsin <mohsinsiddiqui1905@gmail.com>
2024-06-25 18:28:04 +0300

    Capitalization plus timezone

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const records = {
-    position: "web designer",
-    languagesspoken: "English, Hindi, Urdu, Arabic",
+    current_position: "Web Designer",
+    languages_spoken: "English, Hindi, Urdu, Arabic",
+    current_timezone: "GMT+10"
 }
